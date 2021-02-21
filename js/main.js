89878cdf5dcb639e20bdd7959173f48c3cef67da
ayub6717 <ayub15-6717@diu.edu.bd>
2021-02-21 21:39:49 +0600

    MRSG

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
 $('#myCarousel').carousel({
-    interval: 3000,
- })
+  interval: 3000,
+})
+
+jQuery(document).on('click', '.mega-dropdown', function (e) {
+  e.stopPropagation()
+})
+
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i
+  }; // add zero in front of numbers < 10
+  return i;
+}
+
+// *****************************Authority*******************************
+
+$(document).ready(function () {
+  $("#testimonial-slider").owlCarousel({
+    items: 1,
+    itemsDesktop: [1000, 1],
+    itemsDesktopSmall: [979, 1],
+    itemsTablet: [769, 1],
+    pagination: true,
+    transitionStyle: "goDown",
+    autoplay: true
+  });
+});
+// *****************************Authority End*******************************
+
+// ***************image gallery index page************
+baguetteBox.run('.tz-gallery');
+// ***********image gallery index page end***********
